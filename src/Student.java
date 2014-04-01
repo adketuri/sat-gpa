f385d682df907c487a751f897266ff691208476a
Andrew Keturi <andrewketuri@gmail.com>
2014-04-01 10:38:54 -0700

    outlines for student and studentreader classes

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+
+public class Student implements Comparable<Student> {
+
+	private float gpa;
+	private int sat;
+	
+	public Student(float gpa, int sat){
+		this.setGpa(gpa);
+		this.setSat(sat);
+	}
+
+	@Override
+	public int compareTo(Student s) {
+		return 0;
+	}
+
+	public float getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(float gpa) {
+		this.gpa = gpa;
+	}
+
+	public int getSat() {
+		return sat;
+	}
+
+	public void setSat(int sat) {
+		this.sat = sat;
+	}
+}
---
+++ src/StudentReader.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class StudentReader {
+
+	private StudentReader(){
+		
+	}
+	
+	public static ArrayList<Student> read(){
+		ArrayList<Student> students = new ArrayList<>();
+		
+		return students;
+	}
+}
